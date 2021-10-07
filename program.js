c4ddd952b58e3572a1bcbce252f4c9a6490f56d4
ubub-u <91772892+ubub-u@users.noreply.github.com>
2021-10-07 09:51:31 -0700

    prettier html and some javascript functionality

--- program.js
+++ program.js
@@ -1,11 +1,15 @@
 const body = document.querySelector("body");
 const box = document.getElementById('box');
 const reset = document.getElementById('reset');
+const textdiv = document.getElementById('textdiv');
+previousnum = -1;
  
 let quotes = [
     {
         quote: 'be yourself; everyone else is already taken',
-        source: 'Oscar Wilde'
+        source: 'Oscar Wilde',
+        year: '1908,',
+        citation: 'newyork times.'
     },
     {
         quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't hanlde me at my worst, then you sure as hell don't deserve me at my best.",
@@ -14,19 +18,51 @@ let quotes = [
     {
         quote: "two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
         source: 'Albert Einsten'
+    },
+    {
+        quote: 'A room without books is like a body without a soul.',
+        source: 'Marcus Tullius Cicero'
+    },
+    {
+        quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+        source: 'Dr. Seuss'
+    },
+    {
+        quote: "Don’t walk in front of me… I may not follow Don’t walk behind me… I may not lead Walk beside me… just be my friend",
+        source: 'Albert Camus'
     }
     
 ];
 
+let colors = ['#00203FFF', '#ADEFD1FF', '#2C5F2D', '#97BC62FF', '#0063B2FF', '#9CC3D5FF']
+
+
+
 reset.addEventListener('click', function(){
-    program();
+    main();
+    changecolors();
 })
 
-function program(){
-    const randomNumber = Math.floor((Math.random() * quotes.length) + 1) - 1;
+function main(){
+    let randomNumber = Math.floor((Math.random() * quotes.length) + 1) - 1;
+    if(randomNumber == previousnum){
+        randomNumber = Math.floor((Math.random() * quotes.length) + 1) - 1;
+    }
     console.log(randomNumber);
 
-    box.innerHTML = `<p><strong>${quotes[randomNumber].quote}</strong></p><span> - ${quotes[randomNumber].source}</span>`;
+    if(randomNumber == 0){
+        textdiv.innerHTML = `<p><strong>${quotes[randomNumber].quote}</strong></p><span> - ${quotes[randomNumber].source}</span><span> ${quotes[randomNumber].year} ${quotes[randomNumber].citation}</span>`;
+    } else {
+        textdiv.innerHTML = `<p><strong>${quotes[randomNumber].quote}</strong></p><span> - ${quotes[randomNumber].source}</span>`;
+    }
+    previousnum = randomNumber
 }
 
-program();
+function changecolors(){
+    const randomNumber = Math.floor((Math.random() * colors.length) + 1) - 1;
+    body.style.backgroundColor = colors[randomNumber];
+    reset.style.backgroundColor = colors[randomNumber];
+}
+
+main();
+body.style.backgroundColor = '#f98866'
